9e9cb9b8d6c9857e76b75d8e24930cc5b974e0fe
Abdalla Amr Ahmed Elsayed <139905493+0Abdalla0@users.noreply.github.com>
2026-05-18 10:37:15 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,15 +23,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // ================= PRODUCTS DATA =================
     let products = [
-        { id: 1, imgurl: "images/gamingChair.png", productName: "Gaming Chair", price: "280$", category: "Furniture", quantity: 0, ava: true},
+        { id: 1, imgurl: "images/gamingChair.png", productName: "Gaming Chair", price: "280$", category: "Furniture", quantity: 0, ava: false},
         { id: 2, imgurl: "images/EaFc24 (1).png", productName: "EA FC 24", price: "100$", category: "Games", quantity: 0, ava: true},
         { id: 3, imgurl: "images/godOfWar.png", productName: "God Of War", price: "170$", category: "Games", quantity: 0, ava: true},
         { id: 4, imgurl: "images/Ps5.png", productName: "PS5", price: "800$", category: "Console", quantity: 0, ava: false },
         { id: 5, imgurl: "images/SpiderMan.png", productName: "Spider-Man", price: "300$", category: "Games", quantity: 0, ava: false},
         { id: 6, imgurl: "images/Xbox.png", productName: "XBOX Series X", price: "750$", category: "Console", quantity: 0, ava: true },
         { id: 7, imgurl: "images/ps5Con.png", productName: "PS5 Controller", price: "80$", category: "Controller", quantity: 0, ava: false },
-        { id: 8, imgurl: "images/xboxcon.png", productName: "XBOX Controller", price: "60$", category: "Controller", quantity: 0, ava: false },
-        { id: 9, imgurl: "images/gamingTShirt.png", productName: "Gaming T-Shirt", price: "25$", category: "T-shirt", quantity: 0, ava: false },
+        { id: 8, imgurl: "images/xboxcon.png", productName: "XBOX Controller", price: "60$", category: "Controller", quantity: 0, ava: true },
+        { id: 9, imgurl: "images/gamingTShirt.png", productName: "Gaming T-Shirt", price: "25$", category: "T-shirt", quantity: 0, ava: true },
     ];
 
     // ================= DRAW PRODUCTS =================
